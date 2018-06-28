8fdbf2e2b7f7f7d3e203fb2f102cc750ab496716
Owen Thomas <owen@owenhthomas.com>
2018-06-28 13:48:23 -0700

    Remove unnecessary type

--- MVVM-Coordinator-Firestore-Example/Services/FirestoreService.swift
+++ MVVM-Coordinator-Firestore-Example/Services/FirestoreService.swift
@@ -10,26 +10,26 @@ import FirebaseFirestore
 
 class FirestoreService {
   static func getUsers(completion: @escaping ([User]) -> Void) -> ListenerRegistration {
-    return get(type: User.self, atPath: nil, completion: completion)
+    return get(atPath: nil, completion: completion)
   }
 
   static func getItems(userPath: DocumentReference, completion: @escaping ([Item]) -> Void) -> ListenerRegistration {
-    return get(type: Item.self, atPath: userPath, completion: completion)
+    return get(atPath: userPath, completion: completion)
   }
 
   static func getConstraints(userPath: DocumentReference, completion: @escaping ([Constraint]) -> Void) -> ListenerRegistration {
-    return get(type: Constraint.self, atPath: userPath, completion: completion)
+    return get(atPath: userPath, completion: completion)
   }
   
   static func getDetails(userPath: DocumentReference, completion: @escaping ([Detail]) -> Void) -> ListenerRegistration {
-    return get(type: Detail.self, atPath: userPath, completion: completion)
+    return get(atPath: userPath, completion: completion)
   }
   
-  static func get<T: FirestoreModel>(type: FirestoreModel.Type, atPath path: DocumentReference?, completion:  @escaping ([T]) -> Void) -> ListenerRegistration {
+  private static func get<T: FirestoreModel>(atPath path: DocumentReference?, completion:  @escaping ([T]) -> Void) -> ListenerRegistration {
     return (path?.collection(T.collectionPath) ?? Firestore.firestore().collection(T.collectionPath))
       .addSnapshotListener { (snapshot, error) in
         if let error = error { print(error); return }
-        guard let snapshot = snapshot else { print("No data found"); return }
+        guard let snapshot = snapshot else { print("No data found"); completion([]); return }
         
         let items: [T] = snapshot.documents.compactMap { T(snapShot: $0) }
         completion(items)
