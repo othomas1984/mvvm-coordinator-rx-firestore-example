9c8abfcc3f8ff2549159c02d4c98e8e64470d527
Owen Thomas <owen@owenhthomas.com>
2018-08-06 23:47:56 -0700

    Fix guard cases

--- MVVM-Coordinator-Firestore-Example/View Models/CreateConstraintViewModel.swift
+++ MVVM-Coordinator-Firestore-Example/View Models/CreateConstraintViewModel.swift
@@ -25,7 +25,10 @@ class CreateConstraintViewModel {
   init(userPath: String, forDetailPath detailPath: String?, delegate: CreateConstraintViewModelDelegate, dataService: DataService = DataService()) {
     addTapped = addTappedSubject.asObserver()
     addTappedSubject.throttle(1, latest: false, scheduler: MainScheduler()).subscribe { event in
-      guard case let .next(optionalName) = event, let name = optionalName, !name.isEmpty else {
+      guard case let .next(optionalName) = event else {
+        return
+      }
+      guard let name = optionalName, !name.isEmpty else {
         delegate.createConstraintViewModelDismiss(); return
       }
       dataService.createConstraint(userPath: userPath, with: name) { constraint in
